d082310de58755ddfaa8c81fd5f9f908f32643ff
tolunaytbk <104521355+tolunaytbk@users.noreply.github.com>
2022-09-12 12:51:51 +0300

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,64 @@
+# Libraries
+library(shiny)
+library(shinythemes)
+library(data.table)
+
+# Read in the RF model
+model <- readRDS("model.rds")
+
+# Get Data
+data <- read.csv("GelirSira.csv")
+data <- data[-1]
+
+ui <- fluidPage(theme = shinytheme("superhero"),
+                
+                # Page header
+                headerPanel('Income Level Predictor'),
+                
+                # Input values
+                sidebarPanel(h3("Parameters"),
+                             sliderInput("Cinsiyet", "Gender:",
+                                         min = 0, max = 1,
+                                         value = 1,
+                                         step = 1),
+                             sliderInput("OkulTipi", "School Type:",
+                                         min = 0, max = 1,
+                                         value = 1, 
+                                         step = 1),
+                             sliderInput("Edebiyat", "Letters Grade:",
+                                         min = 0, max = 100,
+                                         value = mean(data$Edebiyat),
+                                         step = 0.5),
+                             sliderInput("Matematik", "Math Grade:",
+                                         min = 0, max = 100,
+                                         value = mean(data$Matematik),
+                                         step = 0.5),
+                             sliderInput("FenBil", "Science Grade:",
+                                         min = 0, max = 100,
+                                         value = mean(data$FenBil),
+                                         step = 0.5),
+                             sliderInput("Tarih", "History Grade:",
+                                         min = 0, max = 100,
+                                         value = mean(data$Tarih),
+                                         step = 0.5),
+                             
+                             actionButton("submitbutton", "Submit", class = "btn-danger")
+                ),
+                
+                mainPanel(
+                  tags$label(h3('Results')), # Status/Output Text Box
+                  verbatimTextOutput('contents'),
+                  tags$label(h4('Prediction')),
+                  tableOutput('tabledata'), # Prediction results table
+                  tags$label(h4('Coefficients and p-values')),
+                  tableOutput('coeff'),
+                  tags$label(h4('Odds Ratios')),
+                  tableOutput('odds'),
+                  tags$label(h4('Marginal Effects')),
+                  tableOutput('m_effect'),
+                  tags$label(h4('Predicted Probabilities at Mean Levels of Variables')),
+                  tableOutput('meanpp')
+                  
+                  
+                )
+)
